1de020308d8b9c475fa94de19669a55e4826f35a
Artem Ocheredko <galaxie.stern@gmail.com>
2016-10-12 15:44:21 +0300

    HG-67 Add account management (#23)
    * HG-67 Add account management. Add default shop services for shop creation. Add party management todo

--- docker-compose.sh
+++ docker-compose.sh
@@ -12,11 +12,32 @@ services:
     command: /sbin/init
     depends_on:
       - machinegun
+      - shumway
 
   machinegun:
     image: dr.rbkmoney.com/rbkmoney/machinegun:4c29acdcdce065dbba1f3c8ee1683caea837869c
     volumes:
       - ./test/machinegun/sys.config:/opt/machinegun/releases/0.1.0/sys.config
+
+  shumway:
+    image: dr.rbkmoney.com/rbkmoney/shumway:b9487a2313ede02780a90895eb74d43e57b931f6
+    entrypoint: |
+      java
+      -Xmx512m
+      -jar
+      /opt/shumway/shumway-0.0.1-SNAPSHOT.jar
+    command: |
+      --spring.datasource.url=jdbc:postgresql://shumway_psql:5432/shumway
+      --spring.datasource.username=shumway
+      --spring.datasource.password=shumway
+    depends_on:
+      - shumway_psql
+  shumway_psql:
+    image: dr.rbkmoney.com/rbkmoney/postgres:9.6
+    environment:
+      - POSTGRES_DATABASE=shumway
+      - POSTGRES_USER=shumway
+      - POSTGRES_PASSWORD=shumway
 
 networks:
   default:
