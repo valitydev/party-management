aec7c23562db1b4076084cc20c1d7b2ef2053666
Andrew Mayorov <encube.ul@gmail.com>
2017-02-01 21:44:07 +0300

    Implement all the hacks (#63)
    * HG-153: Implement setting transport options up for proxies
    
    * HG-153: Reduce inpector selector during payment init
    
    * HG-151: Implement cost range condition
    
    * HG-151: Reimplement inspector tests using selector
    
    * HG-138: Shitcode up a party-related selectors implementation
    
    * HG-153: Use proxy transport options from the app env
    
    * HG-138: Implement const predicate reduction
    
    * HG-138: Bump to rbkmoney/damsel@61701d2
    
    * HG-138: Bump up dominant and build image
    
    * HG-138: Make compose more insistent on health checking
    
    * HG-152: Bump to rbkmoney/image-service-erlang@13454a9
    
    * HG-138: Bump up dominant again
    
    * HG-153: Make cash range check fail upon misconfiguration

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat <<EOF
-version: '2'
+version: '2.1'
 services:
 
   ${SERVICE_NAME}:
@@ -11,34 +11,48 @@ services:
     working_dir: $PWD
     command: /sbin/init
     depends_on:
-      - machinegun
-      - shumway
+      machinegun:
+        condition: service_healthy
+      shumway:
+        condition: service_healthy
 
   dominant:
-    image: dr.rbkmoney.com/rbkmoney/dominant:42e8d0668a661d5c612c508c94272c895b5cc4b7
+    image: dr.rbkmoney.com/rbkmoney/dominant:9c757f3fdeae26423b777901da528cdd675dfa40
     command: /opt/dominant/bin/dominant foreground
     depends_on:
-      - machinegun
+      machinegun:
+        condition: service_healthy
 
   machinegun:
     image: dr.rbkmoney.com/rbkmoney/machinegun:bde2440a87e8311b6e2db90e915f8efdaa520ba1
     command: /opt/machinegun/bin/machinegun foreground
     volumes:
       - ./test/machinegun/sys.config:/opt/machinegun/releases/0.1.0/sys.config
+    healthcheck:
+      test: "curl http://localhost:8022/"
+      interval: 5s
+      timeout: 1s
+      retries: 12
 
   shumway:
     image: dr.rbkmoney.com/rbkmoney/shumway:ef494632710c3248a7d6a33fcbeb7944ce8fdd31
     restart: always
-    command: |
-      -Xmx512m
-      -jar /opt/shumway/shumway.jar
-      --spring.datasource.url=jdbc:postgresql://shumway-db:5432/shumway
-      --spring.datasource.username=postgres
-      --spring.datasource.password=postgres
+    entrypoint:
+      - java
+      - -Xmx512m
+      - -jar
+      - /opt/shumway/shumway.jar
+      - --spring.datasource.url=jdbc:postgresql://shumway-db:5432/shumway
+      - --spring.datasource.username=postgres
+      - --spring.datasource.password=postgres
     depends_on:
       - shumway-db
-    environment:
-      - SERVICE_NAME=shumway
+    healthcheck:
+      test: "curl http://localhost:8022/"
+      interval: 5s
+      timeout: 1s
+      retries: 12
+
   shumway-db:
     image: dr.rbkmoney.com/rbkmoney/postgres:9.6
     environment:
