45698158b4c0edf19946ffe499f6b4937b9ebf21
Артем <WWW_cool@inbox.ru>
2018-10-03 11:44:21 +0300

    HG-437: Fix anti fraud bypass (#255)

--- docker-compose.sh
+++ docker-compose.sh
@@ -31,7 +31,7 @@ services:
         condition: service_healthy
 
   machinegun:
-    image: dr.rbkmoney.com/rbkmoney/machinegun:1844dff663c24acdcd32f30ae3ea208f5d05a008
+    image: dr.rbkmoney.com/rbkmoney/machinegun:27df9e276102d5c9faf8d1121374c7355d8e2d1b
     command: /opt/machinegun/bin/machinegun foreground
     volumes:
       - ./test/machinegun/config.yaml:/opt/machinegun/etc/config.yaml
