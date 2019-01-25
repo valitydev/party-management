53c432d52f9d700a38c25e60dd22f12425aa3ca0
Evgeny Levenets <levenets.e.e@gmail.com>
2019-01-25 16:02:25 +0300

    DC-104: bump damsel to work with new domain config (#285)
    rbkmoney/damsel@1c28959

--- docker-compose.sh
+++ docker-compose.sh
@@ -17,7 +17,7 @@ services:
         condition: service_healthy
 
   dominant:
-    image: dr.rbkmoney.com/rbkmoney/dominant:6977214081ee82f0aa191f9ed5970d3f4dcd7b47
+    image: dr.rbkmoney.com/rbkmoney/dominant:06cd029f6d94636302c935dc81ca226f2da0dae4
     command: /opt/dominant/bin/dominant foreground
     depends_on:
       machinegun:
