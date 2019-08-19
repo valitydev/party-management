5864fb9e19123751b500b9a3d578bc5775742b41
Boris <bpozdnyakov@gmail.com>
2019-08-19 17:16:16 +0300

    add mobile_commerce; add tests; suspend timeout behaviour (#340)

--- docker-compose.sh
+++ docker-compose.sh
@@ -18,7 +18,7 @@ services:
     mem_limit: 256M
 
   dominant:
-    image: dr2.rbkmoney.com/rbkmoney/dominant:d5789336735502b0bdb3a37c641125b859750e07
+    image: dr2.rbkmoney.com/rbkmoney/dominant:7e1252e60e4965d03458113fd0c89d447f3520c0
     command: /opt/dominant/bin/dominant foreground
     depends_on:
       machinegun:
