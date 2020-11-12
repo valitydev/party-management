3ba836ffb5e866ce94a55f9c9605c5cf60bd47a5
Roman Pushkov <pushkovroman@me.com>
2020-11-12 11:18:16 +0300

    HG-560: routing with pm (#495)
    * add collect routes handler
    
    * add compute provider handling
    
    * remove collect routes handler
    
    * add faulty config test
    
    * return undefined cash range check result
    
    * fix logger call
    
    * run fmt

--- apps/party_management/src/pm_cash_range.erl
+++ apps/party_management/src/pm_cash_range.erl
@@ -24,7 +24,8 @@ is_inside(Cash, CashRange = #domain_CashRange{lower = Lower, upper = Upper}) ->
         {true, false} ->
             {exceeds, upper};
         _ ->
-            error({misconfiguration, {'Invalid cash range specified', CashRange, Cash}})
+            logger:warning("Invalid cash range specified, ~p, ~p", [CashRange, Cash]),
+            undefined
     end.
 
 compare_cash(_, V, {inclusive, V}) ->
